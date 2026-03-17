4d4a0f50125bd23a28aea2fbf61d551905b268e0
alentandara <alen2211@gmail.com>
2026-03-17 18:52:53 +0100

    smjerovi

---
+++ src/services/smjerovi/SmjerPodaci.js
@@ -0,0 +1,22 @@
+export const smjerovi = [
+    {
+        sifra: 1,
+        naziv: 'Hodanje',
+       
+    },
+    {
+        sifra: 2,
+        naziv: 'Vožnja biciklom',
+       
+    },
+     {
+        sifra: 3,
+        naziv: 'Vožnja automobilom',
+       
+    },
+     {
+        sifra: 4,
+        naziv: 'Let zrakoplovom',
+       
+    },
+]
