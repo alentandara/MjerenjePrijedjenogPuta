2b2c93cc9ddb5fa6a03778e95057813ab21e8060
edunovaProgramiranje <84087934+edunovaProgramiranje@users.noreply.github.com>
2026-03-23 17:08:44 +0100

    Ujednačavanje

--- src/pages/tipovi/TipPregled.jsx
+++ src/pages/tipovi/TipPregled.jsx
@@ -1,22 +1,43 @@
 import { useEffect, useState } from "react"
 import TipService from "../../services/tipovi/TipService";
+import { Table } from "react-bootstrap";
 
-export default function TipPregled(){
+export default function TipPregled() {
 
     const [tipovi, setTipovi] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         ucitajTipove();
-    },[])
+    }, [])
 
     async function ucitajTipove() {
-        await TipService.get().then((odgovor)=>{
+        await TipService.get().then((odgovor) => {
             setTipovi(odgovor.data)
         })
     }
 
-    return(
+    return (
+        <>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Naziv</th>
+                        <th>Opis</th>
+                        <th>Akcija</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tipovi && tipovi.map((tip) => (
+                        <tr>
+                            <td>{tip.naziv}</td>
+                            <td>{tip.opis}</td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </>
 
-     
+
     )
 }
